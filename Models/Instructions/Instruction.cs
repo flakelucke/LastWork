568feb746a4024b196e0cf507068f0936efdc950
Kolya Myalik <kolyamyalik0@gmail.com>
2019-02-18 21:50:00 +0300

    Add data moder for instruction and steps

---
+++ Models/Instructions/Instruction.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace LastWork.Models.Instructions
+{
+    public class Instruction
+    {
+        public long InstructionId {get; set; }
+        public string InstructionName { get; set; }
+        public string Description { get; set; }
+
+    }
+}
---
+++ Models/Instructions/InstructionStep.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace LastWork.Models.Instructions
+{
+    public class InstructionStep
+    {
+        public long StepId {get; set; }
+        public string StepName { get; set; }
+        public string StepDescription { get; set; }
+    }
+}
