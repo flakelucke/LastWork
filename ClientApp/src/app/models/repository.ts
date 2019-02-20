f31ccafc23e6b1f1f3d133d837df4f1e0c7b9531
flakelucke <kolyamyalik0@gmail.com>
2019-02-20 12:44:20 +0300

    Add get instruction/s methods

--- ClientApp/src/app/models/repository.ts
+++ ClientApp/src/app/models/repository.ts
@@ -1,20 +1,35 @@
 import { Instruction } from "./instruction-model/instruction.model";
 import { Injectable } from "@angular/core";
-import { Http } from "@angular/http";
+import { Http, RequestMethod, Request, Response } from "@angular/http";
+import { Observable } from "rxjs";
+import "rxjs/add/operator/map";
+
+const instructionsUrl = "/api/instructions";
 
 @Injectable()
-export class Repository
-{
+export class Repository {
+     instruction: Instruction;
+     instructions: Instruction[];
 
     constructor(private http: Http) {
-         this.getInstruction(4);
+        this.getInstructions();
+    }
+
+    getInstructions() {
+        this.sendRequest(RequestMethod.Get, instructionsUrl)
+        .subscribe(response => { this.instructions= response })
     }
 
     getInstruction(id: number) {
-        this.http.get("/api/instructions/" + id)
-        .subscribe(response => this.instruction =response.json());
-        }
+        this.sendRequest(RequestMethod.Get, instructionsUrl + "/" + id)
+            .subscribe(response => { this.instruction = response; });
+    }
 
-        instruction: Instruction;
 
+    private sendRequest(verb: RequestMethod, url: string,
+        data?: any): Observable<any> {
+        return this.http.request(new Request({
+            method: verb, url: url, body: data
+        })).map(response => response.json());
+    }
 }
