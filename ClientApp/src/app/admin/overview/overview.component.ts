2746d54f7d9eb4e92026382560607fd6f983dc14
Kolya Myalik <kolyamyalik0@gmail.com>
2019-02-28 00:11:06 +0300

    Add admin overview

---
+++ ClientApp/src/app/admin/overview/overview.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Repository } from 'src/app/models/repository';
+import { Instruction } from 'src/app/models/instruction-model/instruction.model';
+
+@Component({
+  selector: 'app-overview',
+  templateUrl: './overview.component.html',
+  styleUrls: ['./overview.component.css']
+})
+export class OverviewComponent implements OnInit {
+
+  constructor(private repo: Repository) { }
+
+  get instructions(): Instruction[] {
+    return this.repo.instructions;
+  }
+
+  ngOnInit() {
+  }
+
+}
