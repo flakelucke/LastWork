0a6e6904ff11d1eb7f91f58b7d1625816f1dd561
Kolya Myalik <kolyamyalik0@gmail.com>
2019-02-28 00:11:19 +0300

    Add admin routes

--- ClientApp/src/app/app.routing.ts
+++ ClientApp/src/app/app.routing.ts
@@ -1,11 +1,22 @@
 import { Routes, RouterModule } from "@angular/router";
 import { InstructionTableComponent } from "./structure/instruction-table/instruction-table.component";
 import { InstructionDetailComponent } from "./structure/instruction-detail/instruction-detail.component";
+import { AdminComponent } from "./admin/admin.component";
+import { InstructionAdminComponent } from "./admin/instruction-admin/instruction-admin.component";
+import { OverviewComponent } from "./admin/overview/overview.component";
 
 const routes: Routes = [
+    {
+        path: "admin", component: AdminComponent,
+        children: [
+            { path: "instructions", component: InstructionAdminComponent },
+            { path: "overview", component: OverviewComponent },
+            { path: "", component: OverviewComponent }
+        ]
+    },
     { path: "table", component: InstructionTableComponent },
-    { path: "detail/:id",component: InstructionDetailComponent},
-    { path: "detail", component: InstructionDetailComponent},
-    { path: "", component: InstructionTableComponent }]      
+    { path: "detail/:id", component: InstructionDetailComponent },
+    { path: "detail", component: InstructionDetailComponent },
+    { path: "", component: InstructionTableComponent }]
 
 export const RoutingConfig = RouterModule.forRoot(routes);
