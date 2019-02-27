820dc5e4ac374ecb29c7eba7151d09928a5756b4
Kolya Myalik <kolyamyalik0@gmail.com>
2019-02-27 15:32:51 +0300

    Add delete bind for steps

--- Models/DataContext.cs
+++ Models/DataContext.cs
@@ -1,6 +1,7 @@
 using LastWork.Models.Instructions;
 using LastWork.Models.InstructionSteps;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace LastWork.Models {
     public class DataContext : DbContext {
@@ -10,5 +11,10 @@ namespace LastWork.Models {
         
         public DbSet<Instruction> Instructions { get; set; }
         public DbSet<InstructionStep> Steps { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Instruction>().HasMany<InstructionStep>(x=>x.Steps)
+                .WithOne().OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
