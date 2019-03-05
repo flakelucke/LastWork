e66f063479494376040970211ad967e0c6b7c586
Kolya Myalik <kolyamyalik0@gmail.com>
2019-03-05 17:41:38 +0300

    Guard routes

--- ClientApp/src/app/app.routing.ts
+++ ClientApp/src/app/app.routing.ts
@@ -4,19 +4,31 @@ import { InstructionDetailComponent } from "./structure/instruction-detail/instr
 import { AdminComponent } from "./admin/admin.component";
 import { InstructionAdminComponent } from "./admin/instruction-admin/instruction-admin.component";
 import { OverviewComponent } from "./admin/overview/overview.component";
+import { AuthenticationComponent } from "./auth/authentication/authentication.component";
+import { AuthenticationGuard } from "./auth/authentication.guard";
+import { RegisterComponent } from "./auth/register/register.component";
+import { RoleGuard } from "./auth/role.guard";
+import { UsersAdminComponent } from "./admin/users-admin/users-admin.component";
 
 const routes: Routes = [
+    { path: "login", component: AuthenticationComponent },
+    { path: "register", component: RegisterComponent},
+    { path: "admin", redirectTo: "/admin/overview", pathMatch: "full"},
     {
         path: "admin", component: AdminComponent,
+        canActivateChild: [RoleGuard],
         children: [
             { path: "instructions", component: InstructionAdminComponent },
+            { path: "users", component: UsersAdminComponent},
             { path: "overview", component: OverviewComponent },
             { path: "", component: OverviewComponent }
+            
         ]
     },
-    { path: "table", component: InstructionTableComponent },
-    { path: "detail/:id", component: InstructionDetailComponent },
-    { path: "detail", component: InstructionDetailComponent },
-    { path: "", component: InstructionTableComponent }]
+    { path: "table", component: InstructionTableComponent ,canActivate: [AuthenticationGuard] },
+    { path: "detail/:id", component: InstructionDetailComponent,canActivate: [AuthenticationGuard] },
+    { path: "detail", component: InstructionDetailComponent,canActivate: [AuthenticationGuard] },
+    { path: "", component: InstructionTableComponent,canActivate: [AuthenticationGuard] },
+    { path: "**", component: InstructionTableComponent,canActivate: [AuthenticationGuard]}]
 
 export const RoutingConfig = RouterModule.forRoot(routes);
