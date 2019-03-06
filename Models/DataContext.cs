1fe8f5c0dcf734397cf9a5e7bd1a8d7e05d76001
Kolya Myalik <kolyamyalik0@gmail.com>
2019-03-06 15:35:20 +0300

    update context to work with identity

--- Models/DataContext.cs
+++ Models/DataContext.cs
@@ -1,19 +1,22 @@
 using LastWork.Models.Instructions;
 using LastWork.Models.InstructionSteps;
+using LastWork.Models.Users;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
-namespace LastWork.Models {
-    public class DataContext : DbContext {
-
+namespace LastWork.Models
+{
+    public class DataContext : IdentityDbContext<User>
+    {
         public DataContext(DbContextOptions<DataContext> opts)
-            : base(opts) { }
-        
+            : base(opts) {}
         public DbSet<Instruction> Instructions { get; set; }
         public DbSet<InstructionStep> Steps { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder) {
-            modelBuilder.Entity<Instruction>().HasMany<InstructionStep>(x=>x.Steps)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Instruction>().HasMany<InstructionStep>(x => x.Steps)
                 .WithOne().OnDelete(DeleteBehavior.Cascade);
         }
     }
